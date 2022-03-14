8b9c45290eb1a8fa8b793e87658c2f2245f3a030
WambuiMbugua <wambuidavies@gmail.com>
2022-03-14 13:20:37 +0300

    add prototype style of fullname

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // business logic
 function Contact(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+  this.first = firstName;
+  this.last = lastName;
 }
+
+Contact.prototype.fullName = function () {
+  return this.first + " " + this.last;
+};
 
 // user interface logic
 
@@ -15,10 +19,19 @@ $(document).ready(function () {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contacts").append(
-      "<li><span class='contact'>" + newContact.firstName + "</span></li>"
+      "<li><span class='contact'>" + newContact.fullName() + "</span></li>"
     );
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+
+    $(".contact")
+      .last()
+      .click(function () {
+        $("#show-contact").show();
+        $("#show-contact h4").text(newContact.firstName);
+        $(".first-name").text(newContact.first);
+        $(".last-name").text(newContact.last);
+      });
   });
 });
